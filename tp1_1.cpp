741cb19cd5b2d921c023ee007be2eaba6a2876b2
lautaro anazgo <lautaroanazgo@gmail.com>
2025-03-31 23:08:58 -0300

    commit final

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
 int main () {
+    int *puntero, num=100;
+    puntero = &num;
     printf("hola mundo");
+
+    printf("\nContenido del puntero: %d", *puntero);
+    printf("\nDireccion almacenada en el puntero: %p", puntero);
+    printf("\nDireccion de la variable: %p", &num);
+    printf("\nDireccion del puntero: %p", &puntero);
+    printf("\nTamanio ocupado por la variable: %d", sizeof(int));
+    
+    return 0;
 
 }
